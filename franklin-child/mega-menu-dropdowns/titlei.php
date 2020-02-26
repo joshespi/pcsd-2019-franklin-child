be6e328e7ce36807e519b7f24808f478a22f889a
Josh Espinoza <joshespi@gmial.com>
2020-02-26 07:41:20 -0700

    updated title I info with requested information

--- franklin-child/mega-menu-dropdowns/titlei.php
+++ franklin-child/mega-menu-dropdowns/titlei.php
@@ -1,11 +1,10 @@
 				<div class="dropDownColumn">
 					<h3><img src="https://franklin.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
 							<ul>
-								<li class="pdf"><a href="https://franklin.provo.edu/wp-content/uploads/2018/11/11132018-Annual-Review-of-Title-I.pdf">Annual Review of Title I</a></li>
-								<li class="pdf"><a href="https://franklin.provo.edu/wp-content/uploads/2018/11/11132018-Revisio%CC%81n-Anual-de-Ti%CC%81tulo-I.pdf">Revisión Anual de Título I</a></li>
+								<li class="ext"><a href="https://utahschoolgrades.schools.utah.gov/Home/SchoolOverAllPerformance?SchoolID=1226&DistrictID=1222&SchoolNbr=104&SchoolLevel=K8&IsSplitSchool=0">Annual Review of Title I</a></li>
 								<li class="int"><a href="https://franklin.provo.edu/title-i/what-is-title-i/">What is Title I</a></li>				
-							<li class="pdf"><a href="https://franklin.provo.edu/wp-content/uploads/2018/11/Franklin-FY19-Needs-Assessment.pdf">Needs Assessment</a></li>
-							<li class="pdf"><a href="https://franklin.provo.edu/wp-content/uploads/2018/11/Franklin-FY19-School-Improvment-Plan.pdf">Title I School Improvement Plan</a></li>
+							<li class="ext"><a href="https://docs.google.com/document/d/1UTGws6jrxXeGmplDlx7r9mMVigaMiWMJumM_2Cztgmw/edit?usp=sharing">Needs Assessment</a></li>
+							<li class="pdf"><a href="https://franklin.provo.edu/wp-content/uploads/2020/02/Franklin-FY20-School-Improvement-Plans.pdf">Title I School Improvement Plan</a></li>
 						</ul>
 						<p>Title I Coordinator: Brenda Fuller (BrendaF@provo.edu)</p>
 				</div>
@@ -24,9 +23,9 @@
 				<div class="dropDownColumn">
 					<h3><img src="https://franklin.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
 						<ul>
-							<li class="int"><a href="https://utahschoolgrades.schools.utah.gov/Home/SchoolOverAllPerformance?SchoolID=1226&DistrictID=1222&SchoolNbr=104&SchoolLevel=K8&IsSplitSchool=0">School Report Card</a></li>			
+							<li class="ext"><a href="https://utahschoolgrades.schools.utah.gov/Home/SchoolOverAllPerformance?SchoolID=1226&DistrictID=1222&SchoolNbr=104&SchoolLevel=K8&IsSplitSchool=0">School Report Card</a></li>			
 							<li class="int"><a href="https://franklin.provo.edu/title-i/parents-right-to-know/">Parents Right to Know</a></li>
-							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
+							<li class="int"><a href="https://provo.edu/title-i/">District Title I</a></li>
 							
 						</ul>
 				</div>
